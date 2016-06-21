24a0a66f7b4bbebc78c17a7fae3fc67467415152
Suchan An <me@ansuchan.com>
2016-06-21 22:56:48 +0900

    init lectures:detail

--- fastmanager/routes/lectures.js
+++ fastmanager/routes/lectures.js
@@ -14,4 +14,16 @@ router.get('/', function(request, response) {
 });
 
 
+router.get('/:lectureId/', function(request, response) {
+  var lectureId = request.params.lectureId;
+
+  Lecture.getLectureById(lectureId ,function(error, lecture){
+    if (error) throw error;
+    return response.render("lectures/detail", {
+      lecture: lecture
+    });
+  });
+});
+
+
 module.exports = router;
